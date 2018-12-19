0634efb2eb21a4cb37f07bd829637f64bb3ee41a
malcomxcunningham <malcomxcunningham@gmail.com>
2018-12-19 12:05:22 +0000

    /me route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get '/me' => 'application#me'
 
   root to: 'doorkeeper/applications#index'
 end

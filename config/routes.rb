be2ce4d46e7c60e971b247e7f448a9096ab7231b
malcomxcunningham <malcomxcunningham@gmail.com>
2018-12-19 12:51:02 +0000

    removed old me method

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/me' => 'application#me'
-
   namespace :api do
     namespace :v1 do
       # another api routes

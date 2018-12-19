f9c9728e7e0a2f40854cd5d05512447835182a62
malcomxcunningham <malcomxcunningham@gmail.com>
2018-12-19 11:01:37 +0000

    admin custom layout and configuration

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def admin?
+    # ATM only this user is an admin
+    true if email == 'test@test.com'
+  end
 end

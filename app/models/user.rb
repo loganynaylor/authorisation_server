9d65fdc3cc3e5cee37dde53acaf4804832ccfdb7
malcomxcunningham <malcomxcunningham@gmail.com>
2018-12-21 12:38:29 +0000

    doorkeper relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :access_grants, class_name: "Doorkeeper::AccessGrant",
+           foreign_key: :resource_owner_id,
+           dependent: :delete_all # or :destroy if you need callbacks
+
+  has_many :access_tokens, class_name: "Doorkeeper::AccessToken",
+           foreign_key: :resource_owner_id,
+           dependent: :delete_all # or :destroy if you need callbacks
+
   def admin?
     # ATM only this user is an admin
     # TODO: change me

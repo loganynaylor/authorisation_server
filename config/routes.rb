90aad6b8efec6ebeee213e13ddb496f481feaab0
malcomxcunningham <malcomxcunningham@gmail.com>
2019-01-02 13:38:04 +0000

    prepared new controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,10 @@ Rails.application.routes.draw do
 
   resource :ldap_login, only: [:new, :create]
 
-  devise_for :users
+  devise_for :users, controllers: {
+               sessions: 'users/sessions'
+             }
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api do
